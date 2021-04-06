168f041c259a88e883c9df636c683077dee4ff10
Udomkarn Dangyuenyong <toontown_zero@live.com>
2021-04-06 16:35:47 +0700

    Simplify all method in slime blast

--- slimeHuanglong+Yoda.py
+++ slimeHuanglong+Yoda.py
@@ -1,15 +1,15 @@
 import  pyautogui,webbrowser,time,random,sys
 
 
-def selectSummon(summonName):
+def selectName(NameName):
     shot = pyautogui.screenshot()
-    check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+summonName+".PNG")
+    check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+NameName+".PNG")
     if check!=None:
         pyautogui.click(check,duration=random.uniform(0.01,0.5))
     while check == None:
         pyautogui.scroll(-100)
         shot = pyautogui.screenshot()
-        check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+summonName+".PNG")
+        check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+NameName+".PNG")
     pyautogui.click(check,duration=random.uniform(0.01,0.5))
 def clickOK():
     shot = pyautogui.screenshot()
@@ -21,18 +21,18 @@ def clickOK():
         shot = pyautogui.screenshot()
         check = pyautogui.locateOnScreen(".\Screenshot\ok.PNG")
     pyautogui.click(check,duration=random.uniform(0.01,0.5))
-def summonCall(callSummon):
+def NameCall(Name):
     pyautogui.screenshot()
-    check = pyautogui.locateCenterOnScreen(".\Screenshot\summonTab.PNG")
+    check = pyautogui.locateCenterOnScreen(".\Screenshot\NameTab.PNG")
     while check == None:
         pyautogui.screenshot()
-        check = pyautogui.locateCenterOnScreen(".\Screenshot\summonTab.PNG")
+        check = pyautogui.locateCenterOnScreen(".\Screenshot\NameTab.PNG")
     time.sleep(2)
     pyautogui.click(check,duration=random.uniform(0.01,0.5))
-    check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+callSummon+"Call.PNG")
+    check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+Name+"Call.PNG")
     while check == None:
         pyautogui.screenshot()
-        check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+callSummon+"Call.PNG")
+        check = pyautogui.locateCenterOnScreen(".\Screenshot\\"+Name+"Call.PNG")
     pyautogui.click(check,duration=random.uniform(0.01,0.5))
     clickOK()
 def attack():
@@ -42,11 +42,19 @@ def attack():
         pyautogui.screenshot()
         check = pyautogui.locateCenterOnScreen(".\Screenshot\\attack.PNG")
     pyautogui.click(check,duration=random.uniform(0.01,0.5))
-#============================================================
+    time.sleep(2)
+def back():
+    pyautogui.click(pyautogui.locateCenterOnScreen(".\Screenshot\\back.PNG"))
+    time.sleep(2)
+def waitForRaidEnd():
+    while pyautogui.locateCenterOnScreen(".\Screenshot\end.PNG") is None:
+        pyautogui.screenshot()
+
+#======================== Setting ==============================
 #Summon to call
-callSummon = "huanglong"
-#Round
-count = 40
+summonName = "huanglong"
+#How many run
+count = 404040
 #Link
 fight = 'http://game.granbluefantasy.jp/#quest/supporter/400181/4'
 #============================================================
@@ -56,19 +64,21 @@ while i<=count:
     while pyautogui.locateCenterOnScreen(".\Screenshot\\vee.PNG") == None:
         pyautogui.locateCenterOnScreen(".\Screenshot\\vee.PNG")
     pyautogui.click(pyautogui.locateCenterOnScreen(".\Screenshot\\vee.PNG"),duration=0.5)
-    selectSummon(callSummon)
+
+
+    #====================== Fight Start ====================#
+    selectName(Name)
     clickOK()
-    summonCall(callSummon)
-    pyautogui.click(pyautogui.locateCenterOnScreen(".\Screenshot\\back.PNG"))
-    time.sleep(1)
+    NameCall(Name)
+    back()
     attack()
-    time.sleep(1)
-    pyautogui.click(pyautogui.locateCenterOnScreen(".\Screenshot\\back.PNG"))
+    back()
     attack()
-    time.sleep(2)
-    pyautogui.click(pyautogui.locateCenterOnScreen(".\Screenshot\\back.PNG"))
-    while pyautogui.locateCenterOnScreen(".\Screenshot\end.PNG") is None:
-        pyautogui.screenshot()
+    back()
+    waitForRaidEnd()
     webbrowser.open(fight, new=2)
+    #============================= Fight END ================#
+
+
     i+=1
     sys.stdout.write("Round "+str(i)+" Finished\n")
